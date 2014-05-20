602e2152d2224814c6d16c3f94f1a854284f8ca9
abhinav <abhinavgujjar@gmail.com>
2014-05-20 14:07:44 +0530

    'images'

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,37 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
-  	$scope.test = 'Working';
-  }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
+.controller('hotelsController', ['$scope', function($scope) {
 
-  }]);
+	$scope.hotels = [
+	{
+		name : 'Taj Banjara',
+		rate : 15000,
+		location : 'Banjara Hills',
+		image: 'http://i.imgur.com/vmviDYS.jpg'
+	},
+	{
+		name : 'Mariott',
+		rate : 35000,
+		location : 'Tank Bund',
+		image: 'http://i.imgur.com/ib6YKfv.jpg'
+	},
+	{
+		name : 'West Inn',
+		rate : 25000,
+		location : 'Mindspace',
+		image: 'http://i.imgur.com/e6CnwW4.jpg'
+	},
+	{
+		name : 'Novotel',
+		rate : 45000,
+		location : 'Hitech City',
+		image: 'http://i.imgur.com/fsmniCe.jpg'
+	},
+
+	]
+
+}])
+.controller('MyCtrl2', ['$scope', function($scope) {
+
+}]);
